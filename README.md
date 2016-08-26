ec43c8df4c6acaca15a6b2d03efacde3dae1dd03
theNerd247 <noah.harvey247 gm>
2016-08-25 23:48:53 -0400

    init repo

---
+++ README.md
@@ -0,0 +1,19 @@
+MTRE4100
+========
+
+MTRE4100 lab work.
+
+Building Instructions 
+---------------------
+
+Run cmake to configure the build system.
+
+    $ mkdir build && cd build && cmake ..
+	
+Then build everything...
+
+		$ make
+
+or build a single project
+
+		$cd <project-name> && make
---
+++ cmake/FindOctave.cmake
@@ -0,0 +1,32 @@
+#finds octave and any subpackages required
+include(FindPackageHandleStandardArgs)
+
+find_program(Octave_binary "octave")
+
+if(EXISTS ${Octave_binary})
+	find_program(Octave_config_exe "octave-config")
+
+	if(EXISTS ${Octave_config_exe})
+		execute_process(COMMAND ${Octave_config_exe} "-v"
+			OUTPUT_VARIABLE Octave_VERSION_STRING
+			OUTPUT_STRIP_TRAILING_WHITESPACE)
+	endif(EXISTS ${Octave_config_exe})
+endif(EXISTS ${Octave_binary})
+
+#find the components
+foreach(c ${Octave_FIND_COMPONENTS})
+
+	execute_process(COMMAND ${Octave_binary} --eval "pkg list ${c}"
+		OUTPUT_VARIABLE "Octave_${c}_out"
+		)
+
+	if(NOT "${Octave_${c}_out}" MATCHES "package ${c} is not installed.")
+		set("Octave_${c}_FOUND" TRUE)
+	endif()
+endforeach(c ${Octave_FIND_COMPONENTS})
+
+find_package_handle_standard_args(Octave
+	REQUIRED_VARS Octave_binary
+	HANDLE_COMPONENTS
+	VERSION_VAR Octave_VERSION_STRING
+	)
