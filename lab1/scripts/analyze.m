46d71b0b72450d3508683ed0f5928c0f20ef3465
theNerd247 <noah.harvey247 gm>
2016-09-01 09:59:17 -0400

    started working on second part of lab

--- lab1/scripts/analyze.m
+++ lab1/scripts/analyze.m
@@ -5,11 +5,25 @@ pwd
 
 file1 = "../data/2016-08-25_lab1-15hz_1st.csv";
 file2 = "../data/2016-08-25_lab1-30hz_1st.csv";
+file3 = "../data/2016-08-25_lab1-30hz_2nd.csv";
 
 rawData1 = dlmread(file1,',',1,0);
+rawData3 = dlmread(file3,',',1,0);
 
 
 plot(rawData1(:,1), rawData1(:,2));
 %print('../img/foAnalResponse.pdf','-S600,400');
 
-tc1 = interp1(rawData1(:,2),rawData1(:,1),0.632);
+tc1 = interp1(rawData1(:,2),rawData1(:,1),0.632)
+
+y2max = max(rawData3(:,2));
+y2ss = rawData3(columns(rawData3)-1,2);
+
+y2OS = (y2max - 1) / 1 * 100
+
+xi = 1/(sqrt(2));
+y2OSth = 100 * exp(-xi * (pi/sqrt(1-xi^2)));
+
+y2OSerr = (y2OS - y2OSth) / y2OSth * 100
+
+plot(rawData3(:,1),rawData3(:,2));
