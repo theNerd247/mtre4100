c8b93bb3bfa013bd0412fa5c5c09eab18536cc82
theNerd247 <noah.harvey247 gm>
2016-09-08 02:29:11 -0400

    almost done second part.
    I'm not sure what the last couple of bullet points are in the
    connections section.

--- lab2/scripts/analyze.m
+++ lab2/scripts/analyze.m
@@ -1,9 +1,28 @@
+rawData1 = [];
+legends = {};
+
+rawData1 = dlmread('../data/lab2_pt1_src.csv',',',1,1);
+legends{1} = "Square Wave";
+rawData1 = [rawData1,dlmread('../data/lab2_pt1_filter.csv',',',1,1)];
+legends{2} = "Fouier Square Wave";
+
+plot(rawData1);
+title("Square Wave Fourier Transform");
+ylabel("Amplitude");
+xlabel("Time (s)");
+legend(legends);
+
+print("../img/sqrWave.pdf",'-S600,400');
+
+clear all;
+close all;
+
 rawData2 = [];
 legends = {};
 
 for i = 1:4
   file = sprintf("../data/lab2_pt2_n%i.csv",i);
-  rawData2 = [rawData2, dlmread(file,",",1,0)];
+  rawData2 = [rawData2, dlmread(file,",",1,1)];
   legends{i} = sprintf("n = %i",i);
 end
 
@@ -12,3 +31,5 @@ ylabel("Amplitude");
 xlabel("Time (s)");
 title("Sine wave Fourier Decomposition");
 legend(legends);
+
+print('../img/sinWaves.pdf','-S600,400');
